65ab081ae798ba0a13d0d44389d251ba0745248a
germancardenasm <germancardenas@gmail.com>
2019-08-03 17:52:37 -0500

    First commit with main layout

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux'
+import './BurgerBuilder.css'
+
+export default class BurgerBuilder extends Component {
+    
+    render() {
+        return (
+            <Aux>
+                <div>Burger Graphics</div>
+                <div>Build Controls</div>
+            </Aux>
+        )
+    }
+}
