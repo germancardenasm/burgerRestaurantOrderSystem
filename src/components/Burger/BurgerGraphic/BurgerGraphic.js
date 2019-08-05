24b8a46df44bf96cacf88683baae2213c21eca74
germancardenasm <germancardenas@gmail.com>
2019-08-05 06:16:39 -0500

    Create a state in BurgerBuilder and render dinamically the ingredients depending on state

--- src/components/Burger/BurgerGraphic/BurgerGraphic.js
+++ src/components/Burger/BurgerGraphic/BurgerGraphic.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import Ingredient from '../Ingredient/Ingredient'
-import './BurgerGraphic.css'
+import React from "react";
+import Ingredient from "../Ingredient/Ingredient";
+import "./BurgerGraphic.css";
 
-const BurgerGraphic = () => {
-    return (
-        <div className='burger'>
-            <p>This is the burger graphics</p>
-            <Ingredient type="bread_top"/>
-            <Ingredient type="salad"/>
-            <Ingredient type="chesee"/>
-            <Ingredient type="bacon"/>
-            <Ingredient type={2}/>
-            <Ingredient type="bread_bottom"/>
-        </div>
-    )
-}
+const BurgerGraphic = props => {
+  console.log("props", props);
+  const ingCompArr = Object.keys(props.ingredients).map(ingType => {
+    console.log("ingType", ingType);
+    return [...Array(props.ingredients[ingType])].map((_, i) => {
+      return <Ingredient type={ingType} key={ingType + i} />;
+    });
+  });
+  console.log("ingComArr", ingCompArr);
+  return (
+    <div className="burger">
+      <Ingredient type="bread_top" />
+      {ingCompArr}
+      <Ingredient type="bread_bottom" />
+    </div>
+  );
+};
 
-export default BurgerGraphic
+export default BurgerGraphic;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
-import BurgerGraphic from '../../components/Burger/BurgerGraphic/BurgerGraphic'
-import Aux from '../../hoc/Aux'
-import './BurgerBuilder.css'
+import React, { Component } from "react";
+import BurgerGraphic from "../../components/Burger/BurgerGraphic/BurgerGraphic";
+import Aux from "../../hoc/Aux";
+import "./BurgerBuilder.css";
 
 export default class BurgerBuilder extends Component {
-    
-    render() {
-        return (
-            <Aux>
-               <BurgerGraphic />
-                <div>this are the ingredients Controls</div>
-            </Aux>
-        )
+  state = {
+    ingredients: {
+      salad: 1,
+      cheese: 2,
+      bacon: 2,
+      meat: 1
     }
+  };
+
+  render() {
+    return (
+      <Aux>
+        <BurgerGraphic ingredients={this.state.ingredients} />
+        <div>this are the ingredients Controls</div>
+      </Aux>
+    );
+  }
 }
