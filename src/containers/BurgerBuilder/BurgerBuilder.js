3257c2ffb8a3bcd15701eb5dd9e49452095f4f02
germancardenasm <germancardenas@gmail.com>
2019-08-31 21:16:04 -0500

    Add global error handling with HOC

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,152 +1,158 @@
-import React, { Component } from "react";
-import BurgerGraphic from "../../components/Burger/BurgerGraphic/BurgerGraphic";
-import Aux from "../../hoc/Aux";
-import BuildControls from "../../components/Burger/BuildControls/BuildControls";
-import Modal from "../../components/UI/Modal/Modal";
-import prices from "../../assets/prices";
-import "./BurgerBuilder.css";
-import OrderSumary from "../../components/OrderSumary/OrderSumary";
-import axios from "../../order-axios";
-import Spinner from "../../components/UI/Spinner/Spinner";
+import React, { Component } from 'react';
+import BurgerGraphic from '../../components/Burger/BurgerGraphic/BurgerGraphic';
+import Aux from '../../hoc/Aux';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import prices from '../../assets/prices';
+import './BurgerBuilder.css';
+import OrderSumary from '../../components/OrderSumary/OrderSumary';
+import axios from '../../order-axios';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-export default class BurgerBuilder extends Component {
-  state = {
-    ingredients: null,
-    price: 2.5,
-    purchaseAvailable: false,
-    purchasing: false,
-    loading: false
-  };
+class BurgerBuilder extends Component {
+	state = {
+		ingredients: null,
+		price: 2.5,
+		purchaseAvailable: false,
+		purchasing: false,
+		loading: false
+	};
 
-  componentDidMount() {
-    axios
-      .get("https://burgerbuilder-gc.firebaseio.com/ingredients.json")
-      .then(response => {
-        console.dir("REPONSE", response);
-        this.setState({ ingredients: response.data });
-      });
-  }
+	componentDidMount() {
+		axios
+			.get('https://burgerbuilder-gc.firebaseio.com/ingredients.json')
+			.then(response => {
+				console.dir('REPONSE', response);
+				this.setState({ ingredients: response.data });
+			});
+	}
 
-  addIngredients = ing => {
-    this.setState(
-      (state, props) => ({
-        ingredients: {
-          ...state.ingredients,
-          [ing]: state.ingredients[ing] + 1
-        },
-        price: state.price + prices[ing]
-      }),
-      () => this.setPurchaseAvailability()
-    );
-  };
+	addIngredients = ing => {
+		this.setState(
+			(state, props) => ({
+				ingredients: {
+					...state.ingredients,
+					[ing]: state.ingredients[ing] + 1
+				},
+				price: state.price + prices[ing]
+			}),
+			() => this.setPurchaseAvailability()
+		);
+	};
 
-  removeIngredients = ing => {
-    if (this.state.ingredients[ing] === 0) return;
-    this.setState(
-      (state, props) => ({
-        ingredients: {
-          ...state.ingredients,
-          [ing]: state.ingredients[ing] - 1
-        },
-        price: state.price - prices[ing]
-      }),
-      () => this.setPurchaseAvailability()
-    );
-  };
+	removeIngredients = ing => {
+		if (this.state.ingredients[ing] === 0) return;
+		this.setState(
+			(state, props) => ({
+				ingredients: {
+					...state.ingredients,
+					[ing]: state.ingredients[ing] - 1
+				},
+				price: state.price - prices[ing]
+			}),
+			() => this.setPurchaseAvailability()
+		);
+	};
 
-  setPurchaseAvailability = () => {
-    const ingredients = Object.keys(this.state.ingredients).reduce(
-      (acc, curr) => {
-        return acc + this.state.ingredients[curr];
-      },
-      0
-    );
-    if (ingredients) {
-      this.setState({ purchaseAvailable: true });
-    } else {
-      this.setState({ purchaseAvailable: false });
-    }
-  };
+	setPurchaseAvailability = () => {
+		const ingredients = Object.keys(this.state.ingredients).reduce(
+			(acc, curr) => {
+				return acc + this.state.ingredients[curr];
+			},
+			0
+		);
+		if (ingredients) {
+			this.setState({ purchaseAvailable: true });
+		} else {
+			this.setState({ purchaseAvailable: false });
+		}
+	};
 
-  purchaseHandler = () => {
-    this.setState({ purchasing: true });
-  };
+	purchaseHandler = () => {
+		this.setState({ purchasing: true });
+	};
 
-  purchaseCancelHandler = () => {
-    this.setState({ purchasing: false });
-  };
+	purchaseCancelHandler = () => {
+		this.setState({ purchasing: false });
+	};
 
-  placeOrder = () => {
-    console.log("purchase");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.price,
-      customer: {
-        name: "German",
-        address: {
-          street: "Street Colombia",
-          zip: "12345",
-          city: "Envigado"
-        },
-        email: "myemail@mail.com"
-      }
-    };
-    axios
-      .post("/orders.json", order)
-      .then(res =>
-        setTimeout(
-          () =>
-            this.setState({
-              loading: false,
-              purchasing: false
-            }),
-          1000
-        )
-      )
-      .catch(error =>
-        this.setState({
-          loading: false,
-          purchasing: false
-        })
-      );
-  };
+	placeOrder = () => {
+		console.log('purchase');
+		this.setState({ loading: true });
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.price,
+			customer: {
+				name: 'German',
+				address: {
+					street: 'Street Colombia',
+					zip: '12345',
+					city: 'Envigado'
+				},
+				email: 'myemail@mail.com'
+			}
+		};
+		axios
+			.post('/orders.json', order)
+			.then(res =>
+				setTimeout(
+					() =>
+						this.setState({
+							loading: false,
+							purchasing: false
+						}),
+					1000
+				)
+			)
+			.catch(error =>
+				this.setState({
+					loading: false,
+					purchasing: false
+				})
+			);
+	};
 
-  render() {
-    let orderSumary = (
-      <OrderSumary
-        {...this.state}
-        close={this.purchaseCancelHandler}
-        purchase={this.placeOrder}
-      />
-    );
-    if (this.state.loading) {
-      orderSumary = <Spinner />;
-    }
+	render() {
+		let orderSumary = (
+			<OrderSumary
+				{...this.state}
+				close={this.purchaseCancelHandler}
+				purchase={this.placeOrder}
+			/>
+		);
+		if (this.state.loading) {
+			orderSumary = <Spinner />;
+		}
 
-    let burgerComponentes = <Spinner />;
+		let burgerComponentes = <Spinner />;
 
-    if (this.state.ingredients) {
-      burgerComponentes = (
-        <Aux>
-          <BurgerGraphic ingredients={this.state.ingredients} />
-          <BuildControls
-            {...this.state}
-            addIngredients={this.addIngredients}
-            removeIngredients={this.removeIngredients}
-            purchase={this.purchaseHandler}
-          />
-        </Aux>
-      );
-    }
+		if (this.state.ingredients) {
+			burgerComponentes = (
+				<Aux>
+					<BurgerGraphic ingredients={this.state.ingredients} />
+					<BuildControls
+						{...this.state}
+						addIngredients={this.addIngredients}
+						removeIngredients={this.removeIngredients}
+						purchase={this.purchaseHandler}
+					/>
+				</Aux>
+			);
+		}
 
-    return (
-      <Aux>
-        <Modal show={this.state.purchasing} close={this.purchaseCancelHandler}>
-          {orderSumary}
-        </Modal>
-        {burgerComponentes}
-      </Aux>
-    );
-  }
+		return (
+			<Aux>
+				<Modal
+					show={this.state.purchasing}
+					close={this.purchaseCancelHandler}
+				>
+					{orderSumary}
+				</Modal>
+				{burgerComponentes}
+			</Aux>
+		);
+	}
 }
+
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+
+import Aux from '../Aux';
+
+export default (WrappedComponent, axios) => {
+	class hocComponent extends Component {
+		state = {
+			error: null
+		};
+		componentDidMount() {
+			axios.interceptors.request.use(req => {
+				this.setState({ error: null });
+				return req;
+			});
+			axios.interceptors.response.use(
+				res => res,
+				error => {
+					this.setState({ error: error });
+					return;
+				}
+			);
+		}
+		errorConfirmedHandler = () => {
+			this.setState({ error: null });
+		};
+		render() {
+			return (
+				<Aux>
+					<Modal
+						show={this.state.error}
+						close={
+							this.state.error ? this.errorConfirmedHandler : null
+						}
+					>
+						{this.state.error ? this.state.error.message : null}
+					</Modal>
+					<WrappedComponent {...this.props} />;
+				</Aux>
+			);
+		}
+	}
+
+	return hocComponent;
+};
