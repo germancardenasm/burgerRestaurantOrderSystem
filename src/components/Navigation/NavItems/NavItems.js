78b113d7c0bdb32c2a6170ae2681aa233c43aecb
germancardenasm <germancardenas@gmail.com>
2019-09-01 10:54:08 -0500

    Delete NavItem component

--- src/components/Navigation/NavItems/NavItems.js
+++ src/components/Navigation/NavItems/NavItems.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
 import NavItem from './NavItem/NavItem';
+import { NavLink } from 'react-router-dom';
 import './NavItems.css';
 
 const NavItems = () => (
 	<ul className='nav_items'>
-		<NavItem link='/' active>
-			Burger Builder
-		</NavItem>
-		<NavItem link='/Checkout'>Checkout</NavItem>
+		<li className='nav_item'>
+			<NavLink to='/' exact>
+				Burger Builder
+			</NavLink>
+		</li>
+		<li className='nav_item'>
+			<NavLink to='/checkout'>Checkout</NavLink>
+		</li>
 	</ul>
 );
 
