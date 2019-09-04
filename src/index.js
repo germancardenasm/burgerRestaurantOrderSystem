26218b748b40ca8ad4656424452e3441ca9d2b8c
germancardenasm <germancardenas@gmail.com>
2019-09-04 14:38:12 -0500

    Add Thunk

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
 import burgerBuilderReducer from './Store/reducers/burgerBuilderReducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
 	burgerBuilderReducer /* preloadedState, */,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
